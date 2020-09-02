e89f273bd87a439d0ac9d1950d3eda88505586e2
Bobby Olejnik <bobby@olejnik.dev>
2020-09-02 13:31:08 +0200

    Updates

---
+++ precache-manifest.b4201c68dfb333e8ad93f93cbabea74f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c3219f3e0532532fcddbdbacaa8f86",
+    "url": "/olejnikdev/index.html"
+  },
+  {
+    "revision": "ae9e674ba583bc7ad7ea",
+    "url": "/olejnikdev/static/css/main.f2dcc3a6.chunk.css"
+  },
+  {
+    "revision": "2194dbc470ea1da083e2",
+    "url": "/olejnikdev/static/js/2.5ec41b3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/olejnikdev/static/js/2.5ec41b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9e674ba583bc7ad7ea",
+    "url": "/olejnikdev/static/js/main.186839e1.chunk.js"
+  },
+  {
+    "revision": "d29588eeeaf8e67304d4",
+    "url": "/olejnikdev/static/js/runtime-main.38b07151.js"
+  }
+]);

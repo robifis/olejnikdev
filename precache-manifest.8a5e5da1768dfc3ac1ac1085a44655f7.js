646e220db93550aff17588d8ced1ee355d08093e
Bobby Olejnik <bobby@olejnik.dev>
2020-09-02 13:27:25 +0200

    Updates

---
+++ precache-manifest.8a5e5da1768dfc3ac1ac1085a44655f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baeac003d18b51866dc64188866aab04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae9e674ba583bc7ad7ea",
+    "url": "/static/css/main.f2dcc3a6.chunk.css"
+  },
+  {
+    "revision": "2194dbc470ea1da083e2",
+    "url": "/static/js/2.5ec41b3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5ec41b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9e674ba583bc7ad7ea",
+    "url": "/static/js/main.186839e1.chunk.js"
+  },
+  {
+    "revision": "00355ff018ed9ba8fb50",
+    "url": "/static/js/runtime-main.6f7f3f67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/olejnikdev/precache-manifest.b4201c68dfb333e8ad93f93cbabea74f.js"
+  "/precache-manifest.8a5e5da1768dfc3ac1ac1085a44655f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/olejnikdev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
